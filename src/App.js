8a68c553a28eb9443b6925d5a3610849948c2449
hien pham <hien.pham@onsolve.com>
2017-08-18 17:22:50 +0700

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PaginationComponent from 'components/PaginationComponent'
+class App extends React.Component {
+  render() {
+    return (
+      <PaginationComponent>
+        1
+      </PaginationComponent>
+    )
+  }
+}
+export default App
